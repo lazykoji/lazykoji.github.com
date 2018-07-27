9a813192981bc47250d0570dbc7efef783f06dab
lazykoji <lazykoji@gmail.com>
2018-07-27 16:44:25 +0800

    add

--- iskoji/js/ycl.js
+++ iskoji/js/ycl.js
@@ -1,6 +1,9 @@
-$(window).ready(function() {
-    $('#loading').hide(5000);
-    $('.menuicon').click(function(){
+// $(window).ready(function() {
+//     $('#loading').hide(5000);
+// });
+
+$(document).ready(function() {
+	$('.menuicon').click(function(){
 		$(this).toggleClass('open');
 		$('.menu').toggleClass('open');
 
@@ -8,4 +11,41 @@ $(window).ready(function() {
     $('.menu > ul > li > a').click(function(){
 		$('.menu , .menuicon').toggleClass('open');
     });
+    // Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
 });
